8650bfe49863331794afa6f98f3ac1246c6a97ca
Ax-Sylvester Hommuk <ax-sylvester.hommuk@khk.ee>
2022-01-27 09:28:25 +0200

    Kasutaja ulesanne

---
+++ praks2/Kasutaja ulesanne - 2/kasutajad.py
@@ -0,0 +1,15 @@
+from kasutaja import Kasutaja
+
+# Inimese andmed
+kasutaja1 = Kasutaja("Ax-Sylvester", "Hommuk")
+kasutaja1.setandmed(17, "ax-sylvester.hommuk@voco.ee")
+
+# Sisselogimised
+kasutaja1.suurenda_sisselogimiskatsed()
+kasutaja1.suurenda_sisselogimiskatsed()
+kasutaja1.suurenda_sisselogimiskatsed()
+
+kasutaja1.tervita_kasutaja()
+kasutaja1.kirjelda_kasutaja()
+
+kasutaja1.reset_sisselogimiskatsed()
