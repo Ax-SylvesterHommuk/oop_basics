51af4c0e3b65b0552f1d10fd377c01a9171f66fc
Ax-Sylvester Hommuk <ax-sylvester.hommuk@khk.ee>
2022-02-07 15:22:26 +0200

    Administraator ja privileegid ülesanne

---
+++ praks2/Administraator ja privileegid/admin.py
@@ -0,0 +1,7 @@
+from kasutaja import Kasutaja
+
+class Admin(Kasutaja):
+    privileegid = []
+
+    def naita_privileegid(self):
+        print(self.privileegid)
---
+++ praks2/Kasutaja andmete muutmine ulesanne/kasutaja_andme_muutja.py
@@ -0,0 +1,5 @@
+from kasutaja import Kasutaja
+
+class Andme_muutja:
+    def __init__(self):
+        self.parool = "qwerty"
